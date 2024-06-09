ceaa5f89b8fc3f0ae8bd316d8c2357f5e57a681b
bkfan1 <jpf8296@gmail.com>
2024-06-09 13:06:14 -0400

    Añade codigo al evento Button1.Click

---
+++ SegundoCorteMDI/PdfEstudianteFormularioMDI.vb
@@ -0,0 +1,7 @@
+﻿Public Class PdfEstudianteFormularioMDI
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Process.Start("https://drive.google.com/file/d/1N5DLisQhZCv3aou9-291aak7P1NHUBD2/view?usp=drivesdk")
+
+    End Sub
+End Class

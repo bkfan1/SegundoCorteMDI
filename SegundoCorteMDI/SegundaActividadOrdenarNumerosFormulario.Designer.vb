b6245eb638be6e0438e878db03bcb1d36b90727e
bkfan1 <jpf8296@gmail.com>
2024-06-18 11:28:15 -0400

    Crear formulario de ordenar números

---
+++ SegundoCorteMDI/SegundaActividadOrdenarNumerosFormulario.Designer.vb
@@ -0,0 +1,219 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class SegundaActividadOrdenarNumerosFormulario
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        InputNumero = New TextBox()
+        BtnAgregarNumero = New Button()
+        BtnNuevoProceso = New Button()
+        BtnMenorIgualQueCincuenta = New Button()
+        BtnMayorQueCincuenta = New Button()
+        BtnCeroYveinte = New Button()
+        RadioAsc = New RadioButton()
+        RadioDesc = New RadioButton()
+        Label2 = New Label()
+        Label3 = New Label()
+        ListaDestino = New ListView()
+        ListaOrigen = New ListView()
+        OrdenamientoGroupBox = New GroupBox()
+        OrdenamientoGroupBox.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI", 9F, FontStyle.Bold)
+        Label1.Location = New Point(30, 37)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(56, 15)
+        Label1.TabIndex = 0
+        Label1.Text = "Numero:"
+        ' 
+        ' InputNumero
+        ' 
+        InputNumero.Location = New Point(116, 34)
+        InputNumero.Name = "InputNumero"
+        InputNumero.Size = New Size(100, 23)
+        InputNumero.TabIndex = 1
+        ' 
+        ' BtnAgregarNumero
+        ' 
+        BtnAgregarNumero.Enabled = False
+        BtnAgregarNumero.Location = New Point(261, 34)
+        BtnAgregarNumero.Name = "BtnAgregarNumero"
+        BtnAgregarNumero.Size = New Size(121, 49)
+        BtnAgregarNumero.TabIndex = 2
+        BtnAgregarNumero.Text = "Agregar a la Lista"
+        BtnAgregarNumero.UseVisualStyleBackColor = True
+        ' 
+        ' BtnNuevoProceso
+        ' 
+        BtnNuevoProceso.Location = New Point(514, 34)
+        BtnNuevoProceso.Name = "BtnNuevoProceso"
+        BtnNuevoProceso.Size = New Size(121, 49)
+        BtnNuevoProceso.TabIndex = 3
+        BtnNuevoProceso.Text = "Nuevo Proceso"
+        BtnNuevoProceso.UseVisualStyleBackColor = True
+        ' 
+        ' BtnMenorIgualQueCincuenta
+        ' 
+        BtnMenorIgualQueCincuenta.Enabled = False
+        BtnMenorIgualQueCincuenta.Location = New Point(410, 128)
+        BtnMenorIgualQueCincuenta.Name = "BtnMenorIgualQueCincuenta"
+        BtnMenorIgualQueCincuenta.Size = New Size(75, 23)
+        BtnMenorIgualQueCincuenta.TabIndex = 4
+        BtnMenorIgualQueCincuenta.Text = "<= 50"
+        BtnMenorIgualQueCincuenta.UseVisualStyleBackColor = True
+        ' 
+        ' BtnMayorQueCincuenta
+        ' 
+        BtnMayorQueCincuenta.Enabled = False
+        BtnMayorQueCincuenta.Location = New Point(410, 172)
+        BtnMayorQueCincuenta.Name = "BtnMayorQueCincuenta"
+        BtnMayorQueCincuenta.Size = New Size(75, 23)
+        BtnMayorQueCincuenta.TabIndex = 5
+        BtnMayorQueCincuenta.Text = "> 50"
+        BtnMayorQueCincuenta.UseVisualStyleBackColor = True
+        ' 
+        ' BtnCeroYveinte
+        ' 
+        BtnCeroYveinte.Enabled = False
+        BtnCeroYveinte.Location = New Point(410, 222)
+        BtnCeroYveinte.Name = "BtnCeroYveinte"
+        BtnCeroYveinte.Size = New Size(75, 23)
+        BtnCeroYveinte.TabIndex = 6
+        BtnCeroYveinte.Text = ">0 y <20"
+        BtnCeroYveinte.UseVisualStyleBackColor = True
+        ' 
+        ' RadioAsc
+        ' 
+        RadioAsc.AutoSize = True
+        RadioAsc.Location = New Point(18, 31)
+        RadioAsc.Name = "RadioAsc"
+        RadioAsc.Size = New Size(87, 19)
+        RadioAsc.TabIndex = 7
+        RadioAsc.TabStop = True
+        RadioAsc.Text = "Ascendente"
+        RadioAsc.UseVisualStyleBackColor = True
+        ' 
+        ' RadioDesc
+        ' 
+        RadioDesc.AutoSize = True
+        RadioDesc.Location = New Point(18, 63)
+        RadioDesc.Name = "RadioDesc"
+        RadioDesc.Size = New Size(93, 19)
+        RadioDesc.TabIndex = 8
+        RadioDesc.TabStop = True
+        RadioDesc.Text = "Descendente"
+        RadioDesc.UseVisualStyleBackColor = True
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Segoe UI", 9F, FontStyle.Bold)
+        Label2.Location = New Point(261, 108)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(76, 15)
+        Label2.TabIndex = 9
+        Label2.Text = "Lista Origen:"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Font = New Font("Segoe UI", 9F, FontStyle.Bold)
+        Label3.Location = New Point(514, 108)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(81, 15)
+        Label3.TabIndex = 10
+        Label3.Text = "Lista Destino:"
+        ' 
+        ' ListaDestino
+        ' 
+        ListaDestino.Enabled = False
+        ListaDestino.Location = New Point(514, 128)
+        ListaDestino.Name = "ListaDestino"
+        ListaDestino.Size = New Size(121, 291)
+        ListaDestino.TabIndex = 11
+        ListaDestino.UseCompatibleStateImageBehavior = False
+        ' 
+        ' ListaOrigen
+        ' 
+        ListaOrigen.Enabled = False
+        ListaOrigen.Location = New Point(261, 128)
+        ListaOrigen.Name = "ListaOrigen"
+        ListaOrigen.Size = New Size(121, 291)
+        ListaOrigen.TabIndex = 12
+        ListaOrigen.UseCompatibleStateImageBehavior = False
+        ' 
+        ' OrdenamientoGroupBox
+        ' 
+        OrdenamientoGroupBox.Controls.Add(RadioAsc)
+        OrdenamientoGroupBox.Controls.Add(RadioDesc)
+        OrdenamientoGroupBox.Enabled = False
+        OrdenamientoGroupBox.Location = New Point(30, 108)
+        OrdenamientoGroupBox.Name = "OrdenamientoGroupBox"
+        OrdenamientoGroupBox.Size = New Size(200, 100)
+        OrdenamientoGroupBox.TabIndex = 13
+        OrdenamientoGroupBox.TabStop = False
+        OrdenamientoGroupBox.Text = "Ordenamiento"
+        ' 
+        ' SegundaActividadOrdenarNumerosFormulario
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(692, 450)
+        Controls.Add(OrdenamientoGroupBox)
+        Controls.Add(ListaOrigen)
+        Controls.Add(ListaDestino)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(BtnCeroYveinte)
+        Controls.Add(BtnMayorQueCincuenta)
+        Controls.Add(BtnMenorIgualQueCincuenta)
+        Controls.Add(BtnNuevoProceso)
+        Controls.Add(BtnAgregarNumero)
+        Controls.Add(InputNumero)
+        Controls.Add(Label1)
+        Name = "SegundaActividadOrdenarNumerosFormulario"
+        Text = "SegundaActividadOrdenarNumerosFormulario"
+        OrdenamientoGroupBox.ResumeLayout(False)
+        OrdenamientoGroupBox.PerformLayout()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents InputNumero As TextBox
+    Friend WithEvents BtnAgregarNumero As Button
+    Friend WithEvents BtnNuevoProceso As Button
+    Friend WithEvents BtnMenorIgualQueCincuenta As Button
+    Friend WithEvents BtnMayorQueCincuenta As Button
+    Friend WithEvents BtnCeroYveinte As Button
+    Friend WithEvents RadioAsc As RadioButton
+    Friend WithEvents RadioDesc As RadioButton
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents ListaDestino As ListView
+    Friend WithEvents ListaOrigen As ListView
+    Friend WithEvents OrdenamientoGroupBox As GroupBox
+End Class
---
+++ SegundoCorteMDI/SegundaActividadOrdenarNumerosFormulario.vb
@@ -0,0 +1,236 @@
+﻿Public Class SegundaActividadOrdenarNumerosFormulario
+
+    ' Implementación de algoritmo de ordenamiento QuickSort:
+
+    ' Función QuickSort
+    Private Sub QuickSort(ByRef arr() As Integer, ByVal low As Integer, ByVal high As Integer, ByVal ascendente As Boolean)
+        If low < high Then
+            Dim pivotIndex As Integer = Partition(arr, low, high, ascendente)
+            QuickSort(arr, low, pivotIndex - 1, ascendente)
+            QuickSort(arr, pivotIndex + 1, high, ascendente)
+        End If
+    End Sub
+
+    ' Función Partition
+    Private Function Partition(ByRef arr() As Integer, ByVal low As Integer, ByVal high As Integer, ByVal ascendente As Boolean) As Integer
+        Dim pivot As Integer = arr(high)
+        Dim i As Integer = low - 1
+
+        For j As Integer = low To high - 1
+            If (ascendente AndAlso arr(j) < pivot) OrElse (Not ascendente AndAlso arr(j) > pivot) Then
+                i += 1
+                Swap(arr, i, j)
+            End If
+        Next
+
+        Swap(arr, i + 1, high)
+        Return i + 1
+    End Function
+
+    ' Función Swap
+    Private Sub Swap(ByRef arr() As Integer, ByVal i As Integer, ByVal j As Integer)
+        Dim temp As Integer = arr(i)
+        arr(i) = arr(j)
+        arr(j) = temp
+    End Sub
+
+    ' Funciones auxiliares:
+
+    ' Función para verificar si un número ya existe en ListaOrigen
+    Private Function ExisteEnListaOrigen(ByVal numero As Integer) As Boolean
+        ' Verifica si el número ya está en ListaOrigen
+        For Each item As ListViewItem In ListaOrigen.Items
+            If Integer.Parse(item.Text) = numero Then
+                Return True
+            End If
+        Next
+        Return False
+    End Function
+
+    ' Función para verificar si un número ya existe en ListaDestino
+    Private Function ExisteEnListaDestino(ByVal numero As Integer) As Boolean
+        ' Verifica si el número ya está en ListaDestino
+        For Each item As ListViewItem In ListaDestino.Items
+            If Integer.Parse(item.Text) = numero Then
+                Return True
+            End If
+        Next
+        Return False
+    End Function
+
+    ' Función para ordenar la lista basada en el parámetro ascendente
+    Private Sub OrdenarLista(ascendente As Boolean)
+        ' Convertir los elementos de ListaOrigen a un array de enteros
+        Dim numeros(ListaOrigen.Items.Count - 1) As Integer
+        For i As Integer = 0 To ListaOrigen.Items.Count - 1
+            numeros(i) = Integer.Parse(ListaOrigen.Items(i).Text)
+        Next
+
+        ' Ordenar el array usando QuickSort
+        QuickSort(numeros, 0, numeros.Length - 1, ascendente)
+
+        ' Mostrar el resultado en ListaDestino
+        ListaDestino.Items.Clear()
+        For Each numero As Integer In numeros
+            ListaDestino.Items.Add(New ListViewItem(numero.ToString()))
+        Next
+    End Sub
+
+    Private Sub ClearListas()
+        ' Limpia lista de origen
+        ListaOrigen.Items.Clear()
+        ListaOrigen.Enabled = False
+
+        ' Limpia lista de destino
+        ListaDestino.Items.Clear()
+        ListaDestino.Enabled = False
+    End Sub
+
+    Private Sub ToggleListas(ByVal enable As Boolean)
+
+        ' Desactiva o activa las listas
+        ListaOrigen.Enabled = enable
+        ListaDestino.Enabled = enable
+    End Sub
+
+
+    Private Sub ToggleBotonesCondiciones(ByVal enable As Boolean)
+
+        ' Desactiva o activa los botones para ordenar segun condiciones lógicas
+        BtnCeroYveinte.Enabled = enable
+        BtnMayorQueCincuenta.Enabled = enable
+        BtnMenorIgualQueCincuenta.Enabled = enable
+    End Sub
+
+    Private Sub ToggleRadioBtns(ByVal enable As Boolean)
+        RadioAsc.Enabled = enable
+        RadioDesc.Enabled = enable
+    End Sub
+
+    Private Sub ToggleGroupBox()
+
+        ' Desmarca el radio de ordenamiento ascendente si está checkado
+        If RadioAsc.Checked Then
+            RadioAsc.Checked = False
+            RadioAsc.Enabled = False
+
+            ' Desmarca el radio de ordenamiento descendente si está checkado
+        ElseIf RadioDesc.Checked Then
+            RadioDesc.Checked = False
+            RadioDesc.Enabled = False
+        End If
+
+        ' Desactiva el GroupBox de los métodos de ordenamiento
+        OrdenamientoGroupBox.Enabled = False
+    End Sub
+
+    ' Codigo de componentes formulario:
+    Private Sub BtnAgregarNumero_Click(sender As Object, e As EventArgs) Handles BtnAgregarNumero.Click
+        Dim NumeroAgregar As Integer
+
+        ' Si el valor introducido en InputNumero es un entero, entonces...
+        If Integer.TryParse(InputNumero.Text, NumeroAgregar) Then
+            ' Si  el valor introducido en InputNumero no está en ListaOrigen, agregarlo
+            If Not ExisteEnListaOrigen(NumeroAgregar) Then
+                ListaOrigen.Items.Add(New ListViewItem(NumeroAgregar.ToString()))
+            End If
+
+            If ListaOrigen.Enabled = False Then
+
+                ' Activa la lista de Origen y Destino
+                ToggleListas(True)
+
+                ' Activa el GroupBox de los métodos de ordenamiento (asc. y desc.)
+                OrdenamientoGroupBox.Enabled = True
+
+                ' Activa los radio btns de los métodos de ordenamiento (asc. y desc.)
+                ToggleRadioBtns(True)
+
+                ' Activa los botones para ordenar segun condiciones lógicas
+                ToggleBotonesCondiciones(True)
+
+
+            End If
+        End If
+    End Sub
+
+    Private Sub BtnNuevoProceso_Click(sender As Object, e As EventArgs) Handles BtnNuevoProceso.Click
+        ' Limpia el input para agregar un número
+        InputNumero.Clear()
+
+        ' Limpia lista de origen y destino
+        ClearListas()
+
+        'Desactiva botón para agregar un número
+        BtnAgregarNumero.Enabled = False
+
+        ' Desactiva botones de condiciones (>=50, <50, 0> y <20)
+        ToggleBotonesCondiciones(False)
+
+        ' Desactiva el GroupBox y los Radio Buttons relacionados con los metodos de ordenamiento (asc. y desc.)
+        ToggleGroupBox()
+
+    End Sub
+
+    Private Sub InputNumero_TextChanged(sender As Object, e As EventArgs) Handles InputNumero.TextChanged
+        Dim NumeroIngresado As Integer
+
+        If Integer.TryParse(InputNumero.Text, NumeroIngresado) Then
+            BtnAgregarNumero.Enabled = True
+        Else
+            BtnAgregarNumero.Enabled = False
+        End If
+    End Sub
+
+    Private Sub BtnMenorIgualQueCincuenta_Click(sender As Object, e As EventArgs) Handles BtnMenorIgualQueCincuenta.Click
+        For Each item As ListViewItem In ListaOrigen.Items
+            Dim numero As Integer
+            ' Intentar convertir el texto del item a entero
+            If Integer.TryParse(item.Text, numero) Then
+                ' Si el número es menor o igual que 50, añadirlo a ListaDestino
+                If numero <= 50 AndAlso Not ExisteEnListaDestino(numero) Then
+                    ListaDestino.Items.Add(New ListViewItem(numero.ToString()))
+                End If
+            End If
+        Next
+    End Sub
+
+    Private Sub BtnMayorQueCincuenta_Click(sender As Object, e As EventArgs) Handles BtnMayorQueCincuenta.Click
+        For Each item As ListViewItem In ListaOrigen.Items
+            Dim numero As Integer
+            ' Intentar convertir el texto del item a entero
+            If Integer.TryParse(item.Text, numero) Then
+                ' Si el número es mayor que 50, añadirlo a ListaDestino
+                If numero > 50 AndAlso Not ExisteEnListaDestino(numero) Then
+                    ListaDestino.Items.Add(New ListViewItem(numero.ToString()))
+                End If
+            End If
+        Next
+    End Sub
+
+    Private Sub BtnCeroYveinte_Click(sender As Object, e As EventArgs) Handles BtnCeroYveinte.Click
+        For Each item As ListViewItem In ListaOrigen.Items
+            Dim numero As Integer
+            ' Intentar convertir el texto del item a entero
+            If Integer.TryParse(item.Text, numero) Then
+                ' Si el número es mayor que 0 y menor que 20, añadirlo a ListaDestino
+                If numero > 0 AndAlso numero < 20 AndAlso Not ExisteEnListaDestino(numero) Then
+                    ListaDestino.Items.Add(New ListViewItem(numero.ToString()))
+                End If
+            End If
+        Next
+    End Sub
+
+    Private Sub RadioAsc_CheckedChanged(sender As Object, e As EventArgs) Handles RadioAsc.CheckedChanged
+        If RadioAsc.Checked Then
+            OrdenarLista(True)
+        End If
+    End Sub
+
+    Private Sub RadioDesc_CheckedChanged(sender As Object, e As EventArgs) Handles RadioDesc.CheckedChanged
+        If RadioDesc.Checked Then
+            OrdenarLista(False)
+        End If
+    End Sub
+
+End Class

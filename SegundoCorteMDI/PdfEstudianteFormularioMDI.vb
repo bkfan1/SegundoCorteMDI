8f50a6ebf1c14414a039c9681ef8d85ac44cc051
bkfan1 <jpf8296@gmail.com>
2024-06-18 16:03:05 -0400

    Añadir lógica en función de evento Button1_Click

--- SegundoCorteMDI/PdfEstudianteFormularioMDI.vb
+++ SegundoCorteMDI/PdfEstudianteFormularioMDI.vb
@@ -1,7 +1,8 @@
 ﻿Public Class PdfEstudianteFormularioMDI
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-
-        Process.Start("https://drive.google.com/file/d/1N5DLisQhZCv3aou9-291aak7P1NHUBD2/view?usp=drivesdk")
+        Process.Start(New ProcessStartInfo("https://drive.google.com/file/d/1N5DLisQhZCv3aou9-291aak7P1NHUBD2/view?usp=drivesdk") With {
+            .UseShellExecute = True
+        })
 
     End Sub
 End Class
